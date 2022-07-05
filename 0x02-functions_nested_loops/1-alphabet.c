afc11fec46cbb4e102fd0609bd8a4e8e8c5897a2
bisrat ashagre <bashagre07@gmail.com>
2022-07-05 12:40:23 -0700

    answer for 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in smallcase
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+
+/**
+ * print_alphabet - prints stuff in amallcase
+ *
+ * return: nothing
+ */
+
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+}
