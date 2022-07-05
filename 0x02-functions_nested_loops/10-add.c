1c56a1c139127c2c6a8a96bfa62c84ac972866fe
bisrat ashagre <bashagre07@gmail.com>
2022-07-05 16:24:58 -0700

    answer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @b: character arguments
+ * @a: character arguments
+ * Return: integer
+ */
+
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
