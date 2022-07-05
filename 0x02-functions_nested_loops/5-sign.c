6dc24f74ccea7620517801af3c42cc9d4aff612d
bisrat ashagre <bashagre07@gmail.com>
2022-07-05 15:07:25 -0700

    answer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign
+ * @n: character argument
+ * Return: 1,0 and -1
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+if (n == 0)
+{
+return (0);
+}
+if (n < 0)
+{
+return (-1);
+}
+}
